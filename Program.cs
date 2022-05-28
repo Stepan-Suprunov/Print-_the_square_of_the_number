30570746225c96cb41ea4325a2527130d35abf0f
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-05-28 19:33:15 +0300

    Add calculations

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿//Запрос на ввод числа
 Console.WriteLine("Введите число ");
 int Num = int.Parse(Console.ReadLine());
+//Возведение в квадрат
+int Square = (Num*Num);
+//Мониторинг
+Console.WriteLine(Square);
